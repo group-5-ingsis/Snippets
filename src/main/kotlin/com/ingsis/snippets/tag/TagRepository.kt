ed00e381b159a93e0c7014da9023235da8bd82db
husseymarcos <mhussey@mail.austral.edu.ar>
2024-10-03 23:10:50 -0300

    remove Tag module

--- src/main/kotlin/com/ingsis/snippets/tag/TagRepository.kt
+++
@@ -1,7 +0,0 @@
-package com.ingsis.snippets.tag
-
-import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.stereotype.Repository
-
-@Repository
-interface TagRepository : JpaRepository<Tag, String>
