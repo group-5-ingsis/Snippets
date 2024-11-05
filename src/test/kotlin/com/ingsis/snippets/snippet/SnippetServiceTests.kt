86a70b8762791a635faa866fe6412f67bfe2cd54
husseymarcos <mhussey@mail.austral.edu.ar>
2024-11-04 23:33:49 -0300

    fix: snippet service tests

--- src/test/kotlin/com/ingsis/snippets/snippet/SnippetServiceTests.kt
+++ src/test/kotlin/com/ingsis/snippets/snippet/SnippetServiceTests.kt
@@ -34,25 +34,48 @@ class SnippetServiceTests {
   fun `should return Snippet created successfully when snippet is created`() {
     val asset = Asset(container = "PrintScript", key = "HelloWorld.ps", content = "HelloWorld!")
 
-    `when`(assetService.createOrUpdateAsset("PrintScript", "HelloWorld.ps", asset))
+    `when`(assetService.createOrUpdateAsset(asset))
       .thenReturn("Snippet created successfully.")
 
-    val result = snippetService.createSnippet(asset)
+    val snippetDto = SnippetDto(
+      author = "1",
+      name = "HelloWorld.ps",
+      description = "my cool snippet",
+      version = "1.1",
+      language = "PrintScript",
+      content = "println(2 + 2);"
+    )
 
-    assertEquals("Snippet created successfully.", result)
-    verify(assetService, times(1)).createOrUpdateAsset("PrintScript", "HelloWorld.ps", asset)
+    val snippet = Snippet(snippetDto)
+
+    `when`(snippetRepository.save(any(Snippet::class.java))).thenReturn(snippet)
+
+    val result = snippetService.createSnippet(snippetDto)
+
+    assertEquals(snippet, result)
   }
 
   @Test
   fun `createOrUpdateSnippet should return Snippet updated successfully when snippet is updated`() {
     val asset = Asset(container = "PrintScript", key = "HelloWorld.ps", content = "HelloWorld!")
 
-    `when`(assetService.createOrUpdateAsset("PrintScript", "HelloWorld.ps", asset))
+    `when`(assetService.createOrUpdateAsset(asset))
       .thenReturn("Snippet updated successfully.")
 
-    val result = snippetService.createSnippet(asset)
+    val snippetDto = SnippetDto(
+      author = "1",
+      name = "HelloWorld.ps",
+      description = "my cool snippet",
+      version = "1.1",
+      language = "PrintScript",
+      content = "println(2 + 2);"
+    )
 
-    assertEquals("Snippet updated successfully.", result)
-    verify(assetService, times(1)).createOrUpdateAsset("PrintScript", "HelloWorld.ps", asset)
+    val snippet = Snippet(snippetDto)
+    `when`(snippetRepository.save(any(Snippet::class.java))).thenReturn(snippet)
+
+    val result = snippetService.createSnippet(snippetDto)
+
+    assertEquals(snippet, result)
   }
 }
