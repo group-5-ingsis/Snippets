feae513a6781aba48aabb57249488d0fac87d40e
Mateo Piccinini <128090505+mPiccinini@users.noreply.github.com>
2024-11-13 20:50:39 -0300

    Change route for test controller fix

--- src/main/kotlin/com/ingsis/snippets/test/TestController.kt
+++ src/main/kotlin/com/ingsis/snippets/test/TestController.kt
@@ -8,9 +8,11 @@ import org.slf4j.LoggerFactory
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
-@RestController("/test")
+@RestController
+@RequestMapping("/test") // Set the base path correctly here
 class TestController(
   private val snippetService: SnippetService,
   private val snippetTestProducer: SnippetTestProducer
