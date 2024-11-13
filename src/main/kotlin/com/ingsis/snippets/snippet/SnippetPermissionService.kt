0fee674d8ca02e90cbbc9f214afbebab38b6c725
Mateo Piccinini <128090505+mPiccinini@users.noreply.github.com>
2024-11-13 16:37:03 -0300

    Rollback

--- src/main/kotlin/com/ingsis/snippets/snippet/SnippetPermissionService.kt
+++ src/main/kotlin/com/ingsis/snippets/snippet/SnippetPermissionService.kt
@@ -7,26 +7,22 @@ import org.springframework.http.MediaType
 import org.springframework.stereotype.Service
 import org.springframework.web.client.RestClientException
 import org.springframework.web.client.RestTemplate
-import org.slf4j.LoggerFactory
 
 @Service
 class SnippetPermissionService(private val restTemplate: RestTemplate) {
 
-  private val logger = LoggerFactory.getLogger(SnippetPermissionService::class.java)
-  private val permissionServiceUrl: String = System.getenv("PERMISSION_SERVICE_URL") ?: "http://default-permission-service-url"
+  private val permissionServiceUrl: String = System.getenv("PERMISSION_SERVICE_URL") ?: "permission"
 
   fun updatePermissions(userId: String, snippetId: String, type: String) {
     val headers = HttpHeaders().apply {
       contentType = MediaType.APPLICATION_JSON
       accept = listOf(MediaType.ALL)
     }
-
     val url = "$permissionServiceUrl/$type/$userId/$snippetId"
     try {
       restTemplate.exchange(url, HttpMethod.POST, HttpEntity<Unit>(null, headers), Void::class.java)
-      logger.info("Permissions updated successfully for userId: $userId, snippetId: $snippetId, type: $type")
-    } catch (ex: RestClientException) {
-      logger.error("Error updating permissions for userId: $userId, snippetId: $snippetId, type: $type", ex)
+    } catch (_: RestClientException) {
+      "Error updating permissions"
     }
   }
 }
