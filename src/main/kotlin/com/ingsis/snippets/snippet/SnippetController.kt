f83b18d5d92ffc9ac746a9fba5283c7a7c30cafb
Mateo Piccinini <128090505+mPiccinini@users.noreply.github.com>
2024-11-13 14:39:42 -0300

    Logging 2

--- src/main/kotlin/com/ingsis/snippets/snippet/SnippetController.kt
+++ src/main/kotlin/com/ingsis/snippets/snippet/SnippetController.kt
@@ -1,5 +1,6 @@
 package com.ingsis.snippets.snippet
 
+import org.slf4j.LoggerFactory
 import org.springframework.security.core.annotation.AuthenticationPrincipal
 import org.springframework.security.oauth2.jwt.Jwt
 import org.springframework.web.bind.annotation.*
@@ -7,37 +8,45 @@ import org.springframework.web.bind.annotation.*
 @RestController
 class SnippetController(private val snippetService: SnippetService) {
 
+  private val logger = LoggerFactory.getLogger(SnippetController::class.java)
+
   @PostMapping("/")
   fun createSnippet(
     @RequestBody snippet: SnippetDto,
     @AuthenticationPrincipal jwt: Jwt
   ): Snippet {
     val userId = jwt.subject
+    logger.info("Creating snippet for userId: $userId")
     return snippetService.createSnippet(userId, snippet)
   }
 
   @GetMapping("/id/{id}")
   fun getSnippetById(@PathVariable id: String): SnippetWithContent {
+    logger.info("Fetching snippet with id: $id")
     return snippetService.getSnippetContent(id)
   }
 
   @GetMapping("/name/{name}")
   fun getSnippetsByName(@PathVariable name: String): List<Snippet> {
+    logger.info("Fetching snippets with name: $name")
     return snippetService.getSnippetsByName(name)
   }
 
   @GetMapping("/")
   fun getAllSnippets(): List<Snippet> {
+    logger.info("Fetching all snippets")
     return snippetService.getSnippets()
   }
 
   @PutMapping("/{id}")
   fun updateSnippet(@PathVariable id: String, @RequestBody updatedSnippet: SnippetDto): Snippet {
+    logger.info("Updating snippet with id: $id")
     return snippetService.updateSnippet(id, updatedSnippet)
   }
 
   @DeleteMapping("/{id}")
   fun deleteSnippet(@PathVariable id: String) {
+    logger.info("Deleting snippet with id: $id")
     snippetService.deleteSnippet(id)
   }
 }
