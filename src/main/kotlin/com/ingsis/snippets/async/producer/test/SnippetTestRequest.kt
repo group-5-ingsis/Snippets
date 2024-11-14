445b5e0340c5381f26a29211a0763444f2300d84
lucasmonteverdi1 <128722688+lucasmonteverdi1@users.noreply.github.com>
2024-11-14 08:57:44 -0300

    update single test request

--- src/main/kotlin/com/ingsis/snippets/async/producer/test/SnippetTestRequest.kt
+++ src/main/kotlin/com/ingsis/snippets/async/producer/test/SnippetTestRequest.kt
@@ -1,7 +1,8 @@
 package com.ingsis.snippets.async.producer.test
 
-/* Contains the location inside the AssetService for the snippet. */
+/* Contains the test id, and the location inside the AssetService for the snippet. */
 data class SnippetTestRequest(
+  val testId: String,
   val container: String,
   val key: String
 )
