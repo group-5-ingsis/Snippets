8791016bf8a6d8427f99458215553e56ccb20c9c
Mateo Piccinini <128090505+mPiccinini@users.noreply.github.com>
2024-11-14 00:59:09 -0300

    Possible Fix SnippetTestProducer

--- src/main/kotlin/com/ingsis/snippets/async/producer/test/SnippetTestProducer.kt
+++ src/main/kotlin/com/ingsis/snippets/async/producer/test/SnippetTestProducer.kt
@@ -5,11 +5,14 @@ import kotlinx.coroutines.reactive.awaitSingle
 import org.austral.ingsis.redis.RedisStreamProducer
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
+import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.data.redis.core.ReactiveRedisTemplate
+import org.springframework.scheduling.annotation.EnableAsync
 import org.springframework.stereotype.Component
 
-@Component
-class SnippetTestProducer @Autowired constructor(
+@SpringBootApplication
+@EnableAsync
+open class SnippetTestProducer @Autowired constructor(
   @Value("\${stream.test}") streamKey: String,
   redis: ReactiveRedisTemplate<String, String>
 ) : RedisStreamProducer(streamKey, redis) {
