7612a0594f63282668ed12f96dd4c5005ef4522b
Mateo Piccinini <128090505+mPiccinini@users.noreply.github.com>
2024-11-14 01:00:32 -0300

    Rollback to possible fix

--- src/main/kotlin/com/ingsis/snippets/async/producer/test/SnippetTestProducer.kt
+++ src/main/kotlin/com/ingsis/snippets/async/producer/test/SnippetTestProducer.kt
@@ -5,20 +5,22 @@ import kotlinx.coroutines.reactive.awaitSingle
 import org.austral.ingsis.redis.RedisStreamProducer
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
-import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.data.redis.core.ReactiveRedisTemplate
-import org.springframework.scheduling.annotation.EnableAsync
 import org.springframework.stereotype.Component
 
-@SpringBootApplication
-@EnableAsync
-open class SnippetTestProducer @Autowired constructor(
+@Component
+class SnippetTestProducer @Autowired constructor(
   @Value("\${stream.test}") streamKey: String,
   redis: ReactiveRedisTemplate<String, String>
 ) : RedisStreamProducer(streamKey, redis) {
 
   suspend fun publishTestRequestEvent(snippet: SnippetTestRequest) {
     val snippetAsJson = JsonUtil.serializeTestToJson(snippet)
+    emit(snippetAsJson).awaitSingle()
+  }
+
+  suspend fun publishCreateTestEvent(snippet: SnippetCreateTestRequest) {
+    val snippetAsJson = JsonUtil.serializeCreateTestToJson(snippet)
     emit(snippetAsJson).awaitSingle()
   }
 }
