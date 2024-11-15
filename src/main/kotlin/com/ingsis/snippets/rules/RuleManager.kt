55cd7b85d7366afbbb1e19e7cf1856e6c086e7e0
husseymarcos <mhussey@mail.austral.edu.ar>
2024-11-15 18:13:57 -0300

    fix RuleManager useless methods

--- src/main/kotlin/com/ingsis/snippets/rules/RuleManager.kt
+++ src/main/kotlin/com/ingsis/snippets/rules/RuleManager.kt
@@ -43,24 +43,4 @@ object RuleManager {
       RuleDto(id = "3", name = "readInputExpressionAllowed", isActive = lintingRules.readInputExpressionAllowed, value = null)
     )
   }
-
-  fun convertToFormattingRules(ruleDtos: List<RuleDto>): FormattingRules {
-    return FormattingRules(
-      spaceBeforeColon = ruleDtos.find { it.name == "spaceBeforeColon" }?.isActive == true,
-      spaceAfterColon = ruleDtos.find { it.name == "spaceAfterColon" }?.isActive == true,
-      spaceAroundAssignment = ruleDtos.find { it.name == "spaceAroundAssignment" }?.isActive == true,
-      newlineAfterPrintln = ruleDtos.find { it.name == "newlineAfterPrintln" }?.value as? Int ?: 0,
-      blockIndentation = ruleDtos.find { it.name == "blockIndentation" }?.value as? Int ?: 0,
-      ifBraceSameLine = ruleDtos.find { it.name == "if-brace-same-line" }?.isActive == true,
-      singleSpaceSeparation = ruleDtos.find { it.name == "mandatory-single-space-separation" }?.isActive == true
-    )
-  }
-
-  fun convertToLintingRules(ruleDtos: List<RuleDto>): LintingRules {
-    return LintingRules(
-      identifierNamingConvention = ruleDtos.find { it.name == "identifierNamingConvention" }?.value as? String ?: "snake-case",
-      printlnExpressionAllowed = ruleDtos.find { it.name == "printlnExpressionAllowed" }?.isActive == true,
-      readInputExpressionAllowed = ruleDtos.find { it.name == "readInputExpressionAllowed" }?.isActive == true
-    )
-  }
 }
